40c495c45c65d9fed2d78c3fe1b6e13d4a2c0d92
v-jovan <mr.vrhovac@mail.ru>
2024-05-04 20:36:29 +0200

    Update login component styles and fix login component in header toolbar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -21,10 +21,12 @@ import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
     InputGroupModule,
     InputGroupAddonModule
   ],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  activeIndex: number = 0;
   visible: boolean = false;
   username: string = '';
   password!: string;
@@ -35,5 +37,13 @@ export class LoginComponent {
 
   closeDialog() {
     this.visible = false;
+  }
+
+  goToRegister() {
+    this.activeIndex = 1;
+  }
+
+  goToLogin() {
+    this.activeIndex = 0;
   }
 }

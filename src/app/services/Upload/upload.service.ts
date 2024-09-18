efc4d1bd7190fa26c309f2db792dc751c6314acf
Jovan <52920858+v-jovan@users.noreply.github.com>
2024-09-19 01:46:37 +0200

    Refactor code to remove unused ErrorInterceptorService dependency in UploadService

--- src/app/services/Upload/upload.service.ts
+++ src/app/services/Upload/upload.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api.service';
-import { ErrorInterceptorService } from 'src/app/interceptors/error.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -8,30 +7,18 @@ import { ErrorInterceptorService } from 'src/app/interceptors/error.interceptor'
 export class UploadService {
   private uploadURL = '/upload';
 
-  constructor(
-    private apiService: ApiService,
-    private errorInterceptor: ErrorInterceptorService
-  ) {}
+  constructor(private apiService: ApiService) {}
 
-  async uploadImage(data: FormData): Promise<string> {
-    try {
-      const response = await this.apiService.axios.post<string>(
-        this.uploadURL,
-        data,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
+  async uploadImage(data: FormData) {
+    const response = await this.apiService.axios.post<string>(
+      this.uploadURL,
+      data,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
         }
-      );
-      return response.data;
-    } catch (error) {
-      this.handleError(error as AxiosError);
-      throw error;
-    }
-  }
-
-  private handleError(error: AxiosError): void {
-    this.errorInterceptor.handleError(error);
+      }
+    );
+    return response.data;
   }
 }
